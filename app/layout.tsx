c73e8a1b72c1d6642ea8b369acf6dd0221e06e5c
Eloho Kennedy <kennyduske@gmail.com>
2026-05-13 07:43:34 +0100

    add og tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,6 +32,32 @@ export const metadata: Metadata = {
     apple: [{ url: '/favicon/apple-touch-icon.png', sizes: '180x180' }],
   },
   manifest: '/favicon/site.webmanifest',
+  openGraph: {
+    title:
+      'Eloho Kennedy | Your business deserves more than a pretty homepage.',
+    description:
+      'Professional websites built for business owners. Be found online, get more inquiries, and grow without the tech overwhelm.',
+    url: 'https://eloho.pro', // Replace with your actual domain if different
+    siteName: 'Eloho Kennedy',
+    images: [
+      {
+        url: "/Eloho's profile.png", // A branded screenshot of your site
+        width: 1200,
+        height: 630,
+        alt: 'Eloho Kennedy Portfolio',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Eloho Kennedy | Professional Web Developer',
+    description:
+      'I build websites that help small businesses look professional and get more inquiries.',
+    images: ['/landing-page.png'],
+    creator: '@elohokennedy', // Replace with your actual Twitter handle
+  },
 };
 
 export const viewport: Viewport = {
