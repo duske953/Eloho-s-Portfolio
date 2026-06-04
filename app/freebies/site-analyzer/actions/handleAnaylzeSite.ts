45f2a559f9e7d98291c9b0e09b1dc9a7f71fd13e
Eloho Kennedy <kennyduske@gmail.com>
2026-06-04 14:20:36 +0100

    change backend url

--- app/freebies/site-analyzer/actions/handleAnaylzeSite.ts
+++ app/freebies/site-analyzer/actions/handleAnaylzeSite.ts
@@ -1,25 +1,9 @@
+import handlePostBackend from '~/utils/handlePostBackend';
+
 export async function handleAnalyzeSite(url: string) {
   try {
-    const data = await fetch(
-      'https://freebies-server-nine.vercel.app/analyze-site',
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          url,
-        }),
-      },
-    );
-    const result = await data.json();
-
-    return { response: result, code: data.status };
-  } catch (error) {
-    return {
-      response:
-        "Something went wrong. We can't seem to reach this site. Please try again later.",
-      code: 200,
-    };
+    return await handlePostBackend('analyze-site', url);
+  } catch (error: any) {
+    return { response: error.message, code: 500 };
   }
 }
---
+++ app/utils/handlePostBackend.ts
@@ -0,0 +1,24 @@
+'use server';
+
+export default async function handlePostBackend(resource: string, url: string) {
+  try {
+    const data = await fetch(
+      `https://freebies-server-nine.vercel.app/api/v1/${resource}`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          url,
+        }),
+      },
+    );
+    const result = await data.json();
+    return { response: result, code: data.status };
+  } catch (error) {
+    throw Error(
+      "Something went wrong. We can't seem to reach this site. Please try again later.",
+    );
+  }
+}
