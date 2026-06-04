dc917db7804eff60a0740636657a5057aecd033a
Eloho Kennedy <kennyduske@gmail.com>
2026-06-04 19:40:36 +0100

    change og tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,7 @@ export const metadata: Metadata = {
     siteName: 'eloho.dev',
     images: [
       {
-        url: "/Eloho's profile.png",
+        url: '/Eloho-OG.png',
         width: 1200,
         height: 630,
         alt: 'Eloho Kennedy Portfolio',
